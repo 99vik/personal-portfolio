508ba4f5ee418caddaf2d3d5ebd3ff5724cdb4ed
Viktor Miric <viktorm47@gmail.com>
2023-11-13 20:06:06 +0100

    Skills component with icons and technologies

---
+++ src/scripts/ImagesExports.js
@@ -0,0 +1,33 @@
+import JavaScriptIcon from '../assets/skills-icons/javascript.png';
+import RubyIcon from '../assets/skills-icons/ruby.png';
+import PythonIcon from '../assets/skills-icons/python.png';
+import HTMLIcon from '../assets/skills-icons/html.png';
+import SQLIcon from '../assets/skills-icons/sql.png';
+import CSSIcon from '../assets/skills-icons/css.png';
+import RailsIcon from '../assets/skills-icons/rails.png';
+import ReactIcon from '../assets/skills-icons/react.png';
+import PostgreSQLIcon from '../assets/skills-icons/postgresql.png';
+import TailwindIcon from '../assets/skills-icons/tailwind.png';
+import GitIcon from '../assets/skills-icons/git.png';
+import GithubIcon from '../assets/skills-icons/github.png';
+import ViteIcon from '../assets/skills-icons/vite.png';
+import WebpackIcon from '../assets/skills-icons/webpack.png';
+import VSCodeIcon from '../assets/skills-icons/vscode.png';
+
+export {
+  JavaScriptIcon,
+  PythonIcon,
+  RubyIcon,
+  HTMLIcon,
+  SQLIcon,
+  CSSIcon,
+  RailsIcon,
+  ReactIcon,
+  PostgreSQLIcon,
+  TailwindIcon,
+  GitIcon,
+  GithubIcon,
+  ViteIcon,
+  WebpackIcon,
+  VSCodeIcon,
+};
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,8 +1,133 @@
+import '../scripts/ImagesExports.js';
+import {
+  CSSIcon,
+  GitIcon,
+  GithubIcon,
+  HTMLIcon,
+  JavaScriptIcon,
+  PostgreSQLIcon,
+  PythonIcon,
+  RailsIcon,
+  ReactIcon,
+  RubyIcon,
+  SQLIcon,
+  TailwindIcon,
+  VSCodeIcon,
+  ViteIcon,
+  WebpackIcon,
+} from '../scripts/ImagesExports.js';
+
 function Skills() {
   return (
-    <div className="bg-neutral-900 h-screen z-100 relative">
-      <div className="h-[500px] w-[300px] animate-bgLight bg-emerald-500 rounded-full z-50 blur-3xl absolute top-0 right-0"></div>
-      <p>Skills</p>
+    <div className="bg-neutral-900 h-screen z-100 relative flex flex-col justify-center items-center">
+      <div className="h-[500px] w-[300px] animate-bgLight bg-emerald-500 rounded-full z-50 blur-3xl absolute -top-[10%] right-0"></div>
+      <div className="flex w-[70%] flex-col gap-5">
+        <div className="flex items-center">
+          <div className="h-[1px] mt-3 mr-5 flex-1 bg-gradient-to-r from-emerald-900 to bg-emerald-400"></div>
+          <h1 className="text-emerald-400 text-3xl font-semibold">
+            Skills, tools & technologies
+          </h1>
+          <div className="h-[1px] mt-3 ml-5 flex-1 bg-gradient-to-r from-emerald-400 to bg-emerald-300"></div>
+        </div>
+        <div className="bg-black flex text-neutral-400 text-sm font-semibold px-6 py-10 rounded-xl">
+          <div className="flex-1">
+            <h1 className="text-center text-emerald-400 font-semibold text-lg mb-8">
+              Languages
+            </h1>
+            <div className="grid grid-cols-3 justify-items-center gap-y-8">
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={JavaScriptIcon} alt="js logo" className="h-[35px]" />
+                <p>JavaScript</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={RubyIcon} alt="ruby logo" className="h-[35px]" />
+                <p>Ruby</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={PythonIcon} alt="python logo" className="h-[35px]" />
+                <p>Python</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={HTMLIcon} alt="html logo" className="h-[35px]" />
+                <p>HTML</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={SQLIcon} alt="sql logo" className="h-[35px]" />
+                <p>SQL</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={CSSIcon} alt="css logo" className="h-[35px]" />
+                <p>CSS</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1">
+            <h1 className="text-center text-emerald-400 font-semibold text-lg mb-8">
+              Frameworks, libraries and databases
+            </h1>
+            <div className="grid grid-cols-3 justify-items-center gap-y-8">
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={RailsIcon} alt="rails logo" className="h-[35px]" />
+                <p>Rails</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={ReactIcon} alt="react logo" className="h-[35px]" />
+                <p>React</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img
+                  src={TailwindIcon}
+                  alt="tailwind logo"
+                  className="h-[35px]"
+                />
+                <p>Tailwind</p>
+              </div>
+              <p></p>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img
+                  src={PostgreSQLIcon}
+                  alt="postgresql logo"
+                  className="h-[35px]"
+                />
+                <p>PostgreSQL</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1">
+            <h1 className="text-center text-emerald-400 font-semibold text-lg mb-8">
+              Miscellaneous
+            </h1>
+            <div className="grid grid-cols-3 justify-items-center gap-y-8">
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={GitIcon} alt="git logo" className="h-[35px]" />
+                <p>Git</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={GithubIcon} alt="github logo" className="h-[35px]" />
+                <p>Github</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={ViteIcon} alt="vite logo" className="h-[35px]" />
+                <p>Vite</p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 justify-items-center mt-8">
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img
+                  src={WebpackIcon}
+                  alt="webpack logo"
+                  className="h-[35px]"
+                />
+                <p>Webpack</p>
+              </div>
+              <div className="flex flex-col justify-center items-center gap-1">
+                <img src={VSCodeIcon} alt="vscode logo" className="h-[35px]" />
+                <p>VS Code</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
