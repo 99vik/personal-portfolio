31304cd3603dad597edd15128167d18a43f629bf
Viktor Miric <viktorm47@gmail.com>
2023-12-07 10:30:32 +0100

    New build

--- README.md
+++
@@ -1,3 +0,0 @@
-# My developer portfolio
-
-View here: https://99vik.github.io/personal-portfolio/
--- src/utils/content/about.js
+++
@@ -1,6 +0,0 @@
-export const about = [
-  "Hello! I'm Viktor, a mechanical engineering graduate currently in the final stretch of my master's degree.",
-  "In addition to my studies, I've delved into the world of full-stack web development since May 2022 and have been in love with it ever since.",
-  "Outside the realm of academia and coding, you'll find me exploring the latest in technology, constantly seeking new knowledge.",
-  "Committed to lifelong learning, I'm consistently fueled by an insatiable will to acquire new skills and knowledge about the world around me.",
-];
